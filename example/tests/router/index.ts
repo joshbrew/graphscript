59b5833fb76bdca91b93e6414dbb939b6faf95f2
Garrett <gflynn@usc.edu>
2022-07-17 15:24:07 -0700

    Update index.ts

--- example/tests/router/index.ts
+++ example/tests/router/index.ts
@@ -7,12 +7,15 @@ const input = 3
 const router = new Router([treeInfo.tree],{loadDefaultRoutes:false})
 console.log('Router', router)
 
-router.subscribe('subtract', (res) => document.body.innerHTML = `
+router.subscribe('subtract', (res) => {
+    console.log('subscription output', res)
+    document.body.innerHTML = `
     <h2>Router</h2>
     <p><b>Result:</b> ${res}</p>
     <p><b>Expected:</b> ${treeInfo.expected([input])}</p>
     <p><b>Test Passed:</b> ${ res == treeInfo.expected([input])}</p>
-`)
+`
+})
 
 router.run('add', input)
 
