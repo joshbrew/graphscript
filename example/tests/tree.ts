8ae84b2e5894ff69e195b7a20903f2be1f8c810e
Garrett <gflynn@usc.edu>
2022-07-17 18:53:52 -0700

    Updated test

--- example/tests/tree.ts
+++ example/tests/tree.ts
@@ -2,34 +2,39 @@ import { DOMService } from "../../services/dom/DOM.service"
 
 export const operators = {
     add:  (a, b=1) => {
-        console.log('add', a, b)
         return a + b
     },
     subtract: (a, b=1) => {
-        console.log('sub', a, b)
         return a - b
+    },
+    multiply: (a, b=100) => {
+        return a*b
     }
 }
 
 const routes = {
-    subtract: {
-        operator: operators.subtract
-    }
-}
-
-export const tree = {
     add: {
         children: {
             'subtract': true
         },
         operator: operators.add,
-        
+    },
+    multiply: {
+        operator: operators.multiply,
+    },
+}
+
+export const tree = {
+    subtract: {
+        operator: operators.subtract,
+        children: {
+            'multiply': true
+        },
     },
     nested: new DOMService({
         name: 'nested',
         routes,
-        loadDefaultRoutes: false
     })
 }
 
-export const expected = (input:any[]=[]) => operators.subtract(operators.add(input[0], input[1]))
+export const expected = (input:any[]=[]) =>  operators.multiply(operators.subtract(operators.add(input[0], input[1])))
