74cf16f8641f40bc165e9fcaed589a32df53a85f
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-07-27 11:35:00 -0700

    add core bundle, no fluff

---
+++ dist/index.core.d.ts
@@ -0,0 +1,6 @@
+export * from './Graph';
+export * from './services/Service';
+export * from './routers/Router';
+export * from './routers/users/User.router';
+export * from './services/worker/Worker.service';
+export * from './services/unsafe/Unsafe.service';
---
+++ index.core.ts
@@ -0,0 +1,12 @@
+
+export * from './Graph'
+
+export * from './services/Service'
+
+export * from './routers/Router'
+
+export * from './routers/users/User.router'
+
+export * from './services/worker/Worker.service'
+
+export * from './services/unsafe/Unsafe.service'
