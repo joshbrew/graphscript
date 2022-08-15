234a27123118fca848c467026becea87b196dfaa
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-08-15 00:02:25 -0700

    fixed worker canvas functions

--- dist/services/worker/WorkerCanvas.d.ts
+++ dist/services/worker/WorkerCanvas.d.ts
@@ -1,12 +1,28 @@
 export declare const workerCanvasRoutes: {
-    transferCanvas: (canvas: any, worker: Worker | MessagePort, context?: string, drawfn?: string | ((canvas: any, context: any) => void)) => string;
+    transferCanvas: (worker: Worker | MessagePort, options: {
+        canvas: HTMLCanvasElement;
+        context?: string;
+        draw?: string | ((canvas: any, context: any) => void);
+        init?: string | ((self: any, canvas: any, context: any) => void);
+        clear?: string | ((self: any, canvas: any, context: any) => void);
+        animating?: boolean;
+    }) => string;
     receiveCanvas: (self: any, origin: any, options: {
-        offscreen: any;
-        _id: string;
+        canvas: any;
         context: string;
-        animation?: string | ((canvas: any, context: any) => void);
-    }) => any;
-    setDraw: (self: any, origin: any, _id: string, drawfn: string | ((canvas: any, context: any) => void)) => boolean;
-    startAnim: (self: any, origin: any, _id: any, drawfn?: string | ((canvas: any, context: any) => void)) => boolean;
-    stopAnim: (self: any, origin: any, _id: any) => boolean;
+        _id?: string;
+        init?: string;
+        draw?: string;
+        clear?: string;
+        animating?: boolean;
+    }) => string;
+    setDraw: (self: any, origin: any, _id: string, draw: string | ((self: any, canvas: any, context: any) => void), init: string | ((self: any, canvas: any, context: any) => void), clear: string | ((self: any, canvas: any, context: any) => void)) => boolean;
+    drawFrame: (self: any, origin: any, _id: string, props?: {
+        [key: string]: any;
+    }) => boolean;
+    setProps: (self: any, origin: any, _id: string, props: {
+        [key: string]: any;
+    }) => boolean;
+    startAnim: (self: any, origin: any, _id: string, draw?: string | ((canvas: any, context: any) => void)) => boolean;
+    stopAnim: (self: any, origin: any, _id: string) => boolean;
 };
--- services/worker/WorkerCanvas.ts
+++ services/worker/WorkerCanvas.ts
@@ -4,83 +4,174 @@ import { parseFunctionFromText } from "../../Graph";
 
 //load on front and backend
 export const workerCanvasRoutes = {
-    transferCanvas:(canvas:any, worker:Worker|MessagePort, context?:string, drawfn?:string|((canvas:any,context:any)=>void)) => {
+    transferCanvas:(
+        worker:Worker|MessagePort,
+        options:{
+            canvas:HTMLCanvasElement,  
+            context?:string, 
+            draw?:string|((canvas:any,context:any)=>void),
+            init?:string|((self,canvas:any,context:any)=>void),
+            clear?:string|((self,canvas:any,context:any)=>void),
+            animating?:boolean //animation will start automatically, else you can call draw conditionally
+        }
+    ) => {
         let _id = `canvas${Math.floor(Math.random()*1000000000000000)}`;
-        let offscreen = canvas.transferControlToOffscreen();
+        let offscreen = (options.canvas as any).transferControlToOffscreen();
 
-        let message:any = {route:'receiveCanvas',args:{offscreen,_id,context}};
+        let message:any = {route:'receiveCanvas',args:{canvas:offscreen,_id, context: options.context}};
 
-        if(drawfn) {
-            if(typeof drawfn === 'function') drawfn = drawfn.toString();
-            message.animation = drawfn;
+        if(options.draw) {
+            if(typeof options.draw === 'function') message.draw = options.draw.toString()
+            else message.draw = options.draw;
+        }
+        if(options.init) {
+            if(typeof options.init === 'function') message.init = options.init.toString()
+            else message.init = options.init;
+        }if(options.clear) {
+            if(typeof options.clear === 'function') message.clear = options.clear.toString()
+            else message.clear = options.clear;
         }
 
         worker.postMessage(message,[offscreen]);
     
         return _id;
     },
-    receiveCanvas:(self, origin, options:{offscreen:any,_id:string,context:string,animation?:string|((canvas:any,context:any)=>void)}) => {
+    receiveCanvas:(
+        self,
+        origin, 
+        options:{
+            canvas:any,
+            context:string,
+            _id?:string,
+            init?:string,
+            draw?:string,
+            clear?:string,
+            animating?:boolean
+        }) => {
         if(!self.graph.CANVASES) self.graph.CANVASES = {};
 
-        self.graph.CANVASES[options._id] = {
-            _id:options._id, 
-            canvas:options.offscreen, 
-            context:options.offscreen.getContext(options.context), 
-            animation:options.animation, 
-            animating:false
+        let canvasOptions = {
+            _id:options._id ? options._id : `canvas${Math.floor(Math.random()*1000000000000000)}`, 
+            canvas:options.canvas, //offscreencanvas which renders to page when transfered from the html canvas 
+            context:options.canvas.getContext(options.context), //get the rendering context based on string passed
+            init:options.init,
+            clear:options.clear,
+            draw:options.draw, 
+            animating:('animating' in options) ? options.animating : true
         };
+
+        self.graph.CANVASES[options._id] = canvasOptions;
         
-        if(typeof self.graph.CANVASES[options._id].animation === 'string') {
-            self.graph.CANVASES[options._id].animation = parseFunctionFromText(self.graph.CANVASES[options._id].animation);
+        if(typeof canvasOptions.draw === 'string') {
+            canvasOptions.draw = parseFunctionFromText(canvasOptions.draw);
+        }       
+        if(typeof canvasOptions.init === 'string') {
+            canvasOptions.init = parseFunctionFromText(canvasOptions.init);
+        }       
+        if(typeof canvasOptions.clear === 'string') {
+            canvasOptions.clear = parseFunctionFromText(canvasOptions.clear);
         }
-        if(typeof self.graph.CANVASES[options._id].animation === 'function') {
-            let draw = (canvas,context) => {            
-                if(self.graph.CANVASES[options._id].animating)
-                    requestAnimationFrame(()=>{ (self.graph.CANVASES[options._id].animation as any)(canvas,context);  })
+        if(typeof canvasOptions.draw === 'function') {
+            let draw = (s,canvas,context) => {            
+                if(s.animating) {
+                    s.draw(s,canvas,context);
+                    requestAnimationFrame(()=>{ 
+                        draw(s,canvas,context);  
+                    });
+                }
             }
 
-            draw(self.graph.CANVASES[options._id].canvas,self.graph.CANVASES[options._id].context);
+            if(typeof canvasOptions.init === 'function') (canvasOptions.init as any)(canvasOptions, canvasOptions.canvas,canvasOptions.context);
+            
+            draw(canvasOptions, canvasOptions.canvas,canvasOptions.context);
+        
         }
    
 
-        return self.graph.CANVASES[options._id];
+        return canvasOptions._id;
     },
-    setDraw:(self, origin, _id:string, drawfn:string|((canvas:any,context:any)=>void))=>{
-        let canvasopts = self.graph.CANVASES[_id];
+    setDraw:(
+        self, 
+        origin, 
+        _id:string, 
+        draw:string|((self,canvas:any,context:any)=>void),
+        init:string|((self,canvas:any,context:any)=>void),
+        clear:string|((self,canvas:any,context:any)=>void)
+    )=>{
+        let canvasopts = self.graph.CANVASES?.[_id];
         if(canvasopts) {
-            if(typeof drawfn === 'string') drawfn = parseFunctionFromText(drawfn);
-            if(typeof drawfn === 'function') {
-                canvasopts.animation = drawfn;
+            if(typeof draw === 'string') draw = parseFunctionFromText(draw);
+            if(typeof draw === 'function') {
+                canvasopts.draw = draw;
+            }
+            if(typeof init === 'string') init = parseFunctionFromText(init);
+            if(typeof init === 'function') {
+                canvasopts.init = init;
+            }
+            if(typeof clear === 'string') clear = parseFunctionFromText(clear);
+            if(typeof clear === 'function') {
+                canvasopts.clear = clear;
             }
             return true;
         }
-        return false;
+        return undefined;
     },
-    startAnim:(self, origin, _id, drawfn?:string|((canvas:any,context:any)=>void))=>{ //run the draw function applied to the animation or provide a new one
-        let canvasopts = self.graph.CANVASES[_id];
-
-        if(canvasopts && drawfn) {
-            if(typeof drawfn === 'string') drawfn = parseFunctionFromText(drawfn);
-            if(typeof drawfn === 'function') {
-                canvasopts.animation = drawfn;
+    drawFrame:(self,origin,_id:string,props?:{[key:string]:any}) => { //can update props when calling draw
+        let canvasopts = self.graph.CANVASES?.[_id];
+        if(canvasopts) {
+            if(props) Object.assign(canvasopts,props);
+            if(canvasopts.draw) {
+                canvasopts.draw(canvasopts,canvasopts.canvas,canvasopts.context);
+                return true;
             }
         }
-
-        if(typeof canvasopts?.animation === 'function' && !canvasopts?.animating) {
-            let draw = (canvas,context) => {            
-                if(canvasopts.animating)
-                    requestAnimationFrame(()=>{ (canvasopts.animation as any)(canvas,context);  })
-            }
-
-            draw(canvasopts.canvas,self.graph.CANVASES[canvasopts._id].context);
+        return undefined;
+    },
+    setProps:(self,origin,_id:string,props:{[key:string]:any}) => { //update animation props, e.g. the radius or color of a circle you are drawing with a stored value
+        let canvasopts = self.graph.CANVASES?.[_id];
+        if(canvasopts) {
+            Object.assign(canvasopts,props);
             return true;
         }
-        return false;
+        return undefined;
     },
-    stopAnim:(self,origin,_id)=>{
-        let canvasopts = self.graph.CANVASES[_id];
-        if(canvasopts) canvasopts.animating = false;
-        return true;
+    startAnim:(self, origin, _id:string, draw?:string|((canvas:any,context:any)=>void))=>{ //run the draw function applied to the animation or provide a new one
+
+        let canvasopts = self.graph.CANVASES?.[_id];
+        canvasopts.animating = true;
+        if(canvasopts && draw) {
+            if(typeof draw === 'string') draw = parseFunctionFromText(draw);
+            if(typeof draw === 'function') {
+                canvasopts.draw = draw;
+            }
+        }
+
+        if(typeof canvasopts?.draw === 'function') {
+            let draw = (s,canvas,context) => {            
+                if(s.animating) {
+                    s.draw(s,canvas,context);
+                    requestAnimationFrame(()=>{ 
+                        draw(s,canvas,context);  
+                    })
+                }
+            }
+
+            if(typeof canvasopts.clear === 'function') (canvasopts.clear as any)(canvasopts, canvasopts.canvas, canvasopts.context);
+            if(typeof canvasopts.init === 'function') (canvasopts.init as any)(canvasopts, canvasopts.canvas, canvasopts.context);
+
+            draw(canvasopts,canvasopts.canvas,canvasopts.context);
+            return true;
+        }
+        return undefined;
+    },
+    stopAnim:(self,origin,_id:string)=>{
+        let canvasopts = self.graph.CANVASES?.[_id];
+        if(canvasopts) {
+            canvasopts.animating = false;
+            if(typeof canvasopts.clear === 'function') canvasopts.clear(canvasopts, canvasopts.canvas, canvasopts.context);
+            return true;
+        }
+        return undefined;
     }
 }
 
