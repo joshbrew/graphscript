8742bd33af1ae72342d6645445779f88677a8d2a
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-08-18 23:29:17 -0700

    Update ECS.service.ts

--- services/ecs/ECS.service.ts
+++ services/ecs/ECS.service.ts
@@ -1,39 +1,40 @@
-import { Graph, GraphNode } from '../../Graph';
-import { Service, ServiceOptions } from '../Service';
+import { GraphNode } from '../../Graph';
+import { Service, ServiceOptions, RouteProp } from '../Service';
 
 //Entity Component System Service
 
 export type ECSOptions = {
     entities:{
         [key:string]:boolean|{
-            components:{
-                [key:string]:boolean
+            components:{ //which systems should call these entities?
+                [key:string]:any //use the system key as the key, value can be a boolean or an object with values etc. use however just helps filter entities
             },
             [key:string]:any
         }
     },
-    components:{
+    systems:{
         [key:string]: boolean|(GraphNode & { operator:(entities:{[key:string]:any})=>any })
     },
-    order:string[] //component order of execution by key
+    order?:string[] //system order of execution by key
 } & ServiceOptions
 
 export class ECSService extends Service {
 
     entities:{
-        [key:string]:GraphNode|{
+        [key:string]:GraphNode|({
             components:{ //entities keep a dict of toggled components that their properties can/should be mutated by, you can ignore it if you don't need it
                 [key:string]:boolean
-            },
-            [key:string]:any
-        }
+            }
+        } & RouteProp)
     } = {}
 
-    components:{
-        [key:string]: boolean|(GraphNode & { operator:(self,origin,entities:{[key:string]:any})=>any }) //operators on components are used to make passes over entitites
+    systems:{
+        [key:string]: boolean|(RouteProp & { operator:(self,origin,entities:{[key:string]:any})=>any })|GraphNode //operators on components are used to make passes over entitites
     } = {}
 
     order:string[]=[] //order of execution for component updates
+
+    animating=true;
 
     constructor(options:ECSOptions) {
         super(options);
@@ -50,33 +51,61 @@ export class ECSService extends Service {
                         (options.entities[key] as any).components = {}
                     }
 
-                    if(!(options.entities[key] instanceof GraphNode)) {
-                        this.add(options.entities[key] as any)
-                    }
                 }
             }
 
-        if(options.components)
-            for(const key in options.components) {
-                if(typeof options.components[key] !== 'object') {
-                    options.components[key] = this.nodes.get(key);
-                    if(!options.components[key]) delete options.components[key];
+        if(options.systems)
+            for(const key in options.systems) {
+                if(typeof options.systems[key] !== 'object') {
+                    options.systems[key] = this.nodes.get(key);
+                    if(!options.systems[key]) delete options.systems[key];
                 }
-                if(typeof options.components[key] === 'object') {
-                    if(!(options.components[key] instanceof GraphNode)) {
-                        this.add(options.components[key] as any)
-                    }
+                if(typeof options.systems[key] === 'object') {
                 }
             }
+
+        if(!options.order) options.order = Object.keys(options.systems);
+        this.order = options.order;
+
+        this.load(this.entities);
+        this.load(this.systems);
     }
 
     //e.g. run on requestAnimationFrame
-    update = () => {
+    update = (filter?:boolean) => {
         this.order.forEach((k) => {
-            if(this.components[k]) {
-                (this.components[k] as GraphNode).run(this.entities);
+            if(this.systems[k]) {
+                if(filter) {
+                    (this.systems[k] as GraphNode).run(
+                        this.filterObject(this.entities,(key,entity)=>{ 
+                            if(entity.components[k])
+                                return true;
+                        }));
+                } else (this.systems[k] as GraphNode).run(this.entities); //unfiltered, it's faster to handle this in the system with lots of entities
             }
         });
+    }
+
+    animate = (filter?:boolean) => {
+        requestAnimationFrame(()=>{
+            if(this.animating){ 
+                this.update(filter);
+                this.animate(filter);
+            }
+        });
+    }
+
+    stop = () => {
+        this.animating = false;
+    }
+
+    start = (filter?:boolean) => {
+        this.animating = true;
+        this.animate(filter);
+    }
+
+    filterObject(o:{[key:string]:any},filter:(string,any)=>boolean|undefined) {
+        return Object.fromEntries(Object.entries(o).filter(([key,value]) => {filter(key,value)}));
     }
 
     addEntity(
@@ -89,6 +118,8 @@ export class ECSService extends Service {
             entity._id = `entity${Math.floor(Math.random()*1000000000000000)}`;
         }
         this.entities[entity._id] = entity as any;
+
+        this.load({[entity._id]:entity});
     
     }
 
@@ -96,13 +127,15 @@ export class ECSService extends Service {
         prototype:{[key:string]:any}, 
         update:(entities:any)=>any
     ) {
-        const component = Object.assign({},prototype);
-        component.operator = update;
-        if(component._id && this.entities[component._id]) {
-            component._id = `component${Math.floor(Math.random()*1000000000000000)}`;
+        const system = Object.assign({},prototype);
+        system.operator = update;
+        if(system._id && this.entities[system._id]) {
+            system._id = `component${Math.floor(Math.random()*1000000000000000)}`;
         }
 
-        this.components[component._id] = this.add(component as any) as GraphNode;
+        this.systems[system._id] = this.add(system as any) as GraphNode;
+
+        this.load({[system._id]:system});
     }
 
 }
